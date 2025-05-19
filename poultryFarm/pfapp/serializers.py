7c3dcba1a566416561e8b678bd6b1b046762fc63
lucky18077 <lucky@clikzopinnovations.com>
2025-05-19 18:41:28 +0530

    hh Signed-off-by: lucky18077 <lucky@clikzopinnovations.com>

--- poultryFarm/pfapp/serializers.py
+++ poultryFarm/pfapp/serializers.py
@@ -11,9 +11,6 @@ class RecipemainSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipemain
         fields = '__all__'
-        extra_kwargs={
-            'plant_id':{'required':False},
-        }
 
 class BatchDataSerializer(serializers.ModelSerializer):
     class Meta:
